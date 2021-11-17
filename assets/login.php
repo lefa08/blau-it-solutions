62c52d234d3ba82d1ac97ef87071385c74ae8030
lefa08 <63107007+lefa08@users.noreply.github.com>
2021-11-17 04:13:31 -0800

    pkk

---
+++ assets/login.php
@@ -0,0 +1,36 @@
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to Ekurhuleni Youth Portal</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+	<div class="header">
+		<h2>Login</h2>
+	</div>
+	
+	<form method="post" action="login.php">
+
+		<?php include('errors.php'); ?>
+
+		<div class="input-group">
+			<label>Username</label>
+			<input type="text" name="username" >
+		</div>
+		<div class="input-group">
+			<label>Password</label>
+			<input type="password" name="password">
+		</div>
+		<div class="input-group">
+			<button type="submit" class="btn" name="login_user">Login</button>
+		</div>
+		<p>
+			Not yet a member? <a href="register.php">Sign up</a>
+		</p>
+	</form>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+
+</body>
+</html>
